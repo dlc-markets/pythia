45d64d3d8f7f08128a998e71b72e32ac1457c162
Pantamis <theo.pantamis@gmail.com>
2023-11-24 12:52:15 +0100

    feat: all cli var available in env (cli has priority) (#17)
    * feat: all cli var available in env (cli has priority)
    
    * fix: set default number of connections from 8000 to  10
    
    * chore: less jobs
    
    * feat: only one job
    
    ---------
    
    Co-authored-by: Victor Afanassieff <victor@afanassieff.com>

--- src/env.rs
+++ src/env.rs
@@ -23,6 +23,21 @@ pub(super) fn match_postgres_env() -> Result<PgConnectOptions, PythiaError> {
 }
 
 pub(super) fn match_secret_key_env() -> Result<SecretKey, PythiaError> {
-    let secret_key = env::var("ORACLE_SECRET_KEY").map_err(|_e| PythiaError::NoSecretKey)?;
+    let secret_key = env::var("PYTHIA_SECRET_KEY").map_err(|_e| PythiaError::NoSecretKey)?;
     SecretKey::from_str(secret_key.as_str()).map_err(|_e| PythiaError::InvalidSecretKey)
+}
+
+pub(super) fn match_debug_mode_env() -> bool {
+    let debug_mode = env::var("PYTHIA_DEBUG_MODE").unwrap_or("false".to_owned());
+    bool::from_str(debug_mode.as_str()).unwrap_or(false)
+}
+
+pub(super) fn match_port_env() -> u16 {
+    let port = env::var("PYTHIA_PORT").unwrap_or("8000".to_owned());
+    u16::from_str(port.as_str()).unwrap_or(8000)
+}
+
+pub(super) fn match_nb_connection() -> u32 {
+    let max_connections = env::var("PYTHIA_NB_CONNECTION").unwrap_or("10".to_owned());
+    u32::from_str(max_connections.as_str()).unwrap_or(10)
 }
