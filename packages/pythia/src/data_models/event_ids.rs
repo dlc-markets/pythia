c7833126046ea3242896269e81b446bdca067cb2
Pantamis <theo.pantamis@gmail.com>
2025-07-11 17:36:12 +0200

    feat(rust-models): expiry support in data models

--- packages/pythia/src/data_models/event_ids.rs
+++ packages/pythia/src/data_models/event_ids.rs
@@ -1,5 +1,6 @@
 use std::{error::Error, fmt, ops::Deref, str::FromStr};
 
+use arrayvec::ArrayString;
 use chrono::{DateTime, Utc};
 use serde::{de::Visitor, Deserialize, Serialize};
 use sqlx::{
@@ -8,13 +9,26 @@ use sqlx::{
     Database, Postgres,
 };
 
-use crate::data_models::{asset_pair::AssetPair, ArrayString};
+use crate::data_models::{
+    asset_pair::AssetPair,
+    error::ParsingError,
+    expiries::{Expiry, EXPIRY_LENGTH},
+};
 
 use sqlx::prelude::*;
 
+const SPOT_LENGTH: usize = 17;
+const FORWARD_LENGTH: usize = 26;
+const DELIVERY_LENGTH: usize = 16;
+const EXPIRY_OFFSET: usize = 8;
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub enum EventId {
-    Spot(ArrayString<17>),
+    Spot(ArrayString<SPOT_LENGTH>),
+    // INVARIANT: at EXPIRY_OFFSET, there is a valid expiry written
+    Forward(ArrayString<FORWARD_LENGTH>),
+    // INVARIANT: at EXPIRY_OFFSET, there is a valid expiry written
+    Delivery(ArrayString<DELIVERY_LENGTH>),
 }
 
 impl EventId {
@@ -22,7 +36,27 @@ impl EventId {
         Self::Spot(
             format_args!("{pair}{t}", t = timestamp.timestamp())
                 .try_into()
-                .expect("We imposed a length of 17"),
+                .expect("We imposed a length of SPOT_LENGTH"),
+        )
+    }
+
+    pub fn forward_of_expiry_with_pair_at_timestamp(
+        pair: AssetPair,
+        expiry: Expiry,
+        timestamp: DateTime<Utc>,
+    ) -> Self {
+        Self::Forward(
+            format_args!("{pair}_{expiry}f{t}", t = timestamp.timestamp())
+                .try_into()
+                .expect("We imposed a length of FORWARD_LENGTH"),
+        )
+    }
+
+    pub fn delivery_of_expiry_with_pair(pair: AssetPair, expiry: Expiry) -> Self {
+        Self::Delivery(
+            format_args!("{pair}_{expiry}d")
+                .try_into()
+                .expect("We imposed a length of DELIVERY_LENGTH"),
         )
     }
 }
@@ -56,6 +90,8 @@ impl AsRef<str> for EventId {
     fn as_ref(&self) -> &str {
         match self {
             Self::Spot(s) => s.as_ref(),
+            Self::Forward(s) => s.as_ref(),
+            Self::Delivery(s) => s.as_ref(),
         }
     }
 }
@@ -68,7 +104,7 @@ impl fmt::Display for EventId {
 
 impl PartialOrd for EventId {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.as_ref().cmp(other.as_ref()))
+        Some(self.cmp(other))
     }
 }
 
@@ -85,16 +121,55 @@ impl Deref for EventId {
     }
 }
 
+impl TryFrom<EventId> for Expiry {
+    type Error = ParsingError;
+    fn try_from(value: EventId) -> Result<Self, Self::Error> {
+        match value {
+            EventId::Forward(string) => {
+                // INVARIANT used: at EXPIRY_OFFSET, there is a valid expiry written
+                string[EXPIRY_OFFSET..EXPIRY_OFFSET + EXPIRY_LENGTH].parse()
+            }
+            EventId::Delivery(string) => {
+                // INVARIANT used: at EXPIRY_OFFSET, there is a valid expiry written
+                string[EXPIRY_OFFSET..EXPIRY_OFFSET + EXPIRY_LENGTH].parse()
+            }
+            _ => Err(ParsingError::NoExpiryInEventId),
+        }
+    }
+}
+
 impl FromStr for EventId {
-    type Err = usize;
+    type Err = ParsingError;
 
     fn from_str(value: &str) -> Result<Self, Self::Err> {
-        match value.len() {
-            17 => Ok(EventId::Spot(
-                value[..17].parse().expect("Length already checked"),
-            )),
-            l => Err(l),
+        let expiry_range = EXPIRY_OFFSET..EXPIRY_OFFSET + EXPIRY_LENGTH;
+
+        if value.len() < expiry_range.end {
+            return Err(ParsingError::InvalidLength {
+                expected: SPOT_LENGTH,
+                actual: value.len(),
+            });
         }
+
+        // INVARIANT check: at EXPIRY_RANGE, there must be a valid expiry
+        if value[expiry_range].parse::<Expiry>().is_ok() {
+            if let Ok(id) = value.parse::<ArrayString<DELIVERY_LENGTH>>() {
+                return Ok(EventId::Delivery(id));
+            }
+
+            if let Ok(id) = value.parse::<ArrayString<FORWARD_LENGTH>>() {
+                return Ok(EventId::Forward(id));
+            }
+        }
+
+        if let Ok(id) = value.parse::<ArrayString<SPOT_LENGTH>>() {
+            return Ok(EventId::Spot(id));
+        }
+
+        Err(ParsingError::InvalidLength {
+            expected: SPOT_LENGTH,
+            actual: value.len(),
+        })
     }
 }
 
@@ -118,18 +193,102 @@ impl<'de> Deserialize<'de> for EventId {
             type Value = EventId;
 
             fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("a string in the format [asset_pair][unix_timestamp]")
+                formatter.write_str(
+                    "a string in the format [asset_pair](_[expiry][f|d])?[unix_timestamp]?",
+                )
             }
 
             fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
             where
                 E: serde::de::Error,
             {
-                Ok(EventId::Spot(
-                    v.parse().map_err(|_| E::invalid_length(v.len(), &"17"))?,
-                ))
+                v.parse::<EventId>()
+                    .map_err(|l| E::custom(format!("Invalid event id: {l}")))
             }
         }
         deserializer.deserialize_str(EventIdVisitor)
+    }
+}
+
+#[cfg(test)]
+mod tests_event_id {
+    use super::*;
+
+    #[test]
+    fn test_event_id_parse() {
+        let event_id_f1 = "BTC_USD_01JUL25f1717336000".parse::<EventId>().unwrap();
+        let event_id_f2 = "BTC_USD_02JUL25f1717336000".parse::<EventId>().unwrap();
+
+        assert!(matches!(event_id_f1, EventId::Forward(_)));
+        assert!(matches!(event_id_f2, EventId::Forward(_)));
+
+        let event_id_d1 = "BTC_USD_01JUL25d".parse::<EventId>().unwrap();
+        let event_id_d2 = "BTC_USD_02JUL25d".parse::<EventId>().unwrap();
+
+        assert!(matches!(event_id_d1, EventId::Delivery(_)));
+        assert!(matches!(event_id_d2, EventId::Delivery(_)));
+
+        let event_id_s1 = "BTC_USD1717336000".parse::<EventId>().unwrap();
+        let event_id_s2 = "BTC_USD1717336000".parse::<EventId>().unwrap();
+
+        assert!(matches!(event_id_s1, EventId::Spot(_)));
+        assert!(matches!(event_id_s2, EventId::Spot(_)));
+    }
+
+    #[test]
+    fn test_event_id_try_from_expiry() {
+        let event_id_f1 = "BTC_USD_01JUL25f1717336000".parse::<EventId>().unwrap();
+        let expiry = Expiry::try_from(event_id_f1).unwrap();
+        assert_eq!(expiry, "01JUL25".parse::<Expiry>().unwrap());
+
+        let event_id_d1 = "BTC_USD_01JUL25d".parse::<EventId>().unwrap();
+        let expiry = Expiry::try_from(event_id_d1).unwrap();
+        assert_eq!(expiry, "01JUL25".parse::<Expiry>().unwrap());
+
+        let event_id_s1 = "BTC_USD1717336000".parse::<EventId>().unwrap();
+        assert!(Expiry::try_from(event_id_s1).is_err());
+    }
+
+    #[test]
+    fn test_event_id_serde() {
+        let event_id_f1 = "BTC_USD_01JUL25f1717336000".parse::<EventId>().unwrap();
+        let serde_event_id = serde_json::to_string(&event_id_f1).unwrap();
+        assert_eq!(serde_event_id, "\"BTC_USD_01JUL25f1717336000\"");
+
+        let de_ser_event_id: EventId = serde_json::from_str(&serde_event_id).unwrap();
+        assert_eq!(event_id_f1, de_ser_event_id);
+
+        let event_id_d1 = "BTC_USD_01JUL25d".parse::<EventId>().unwrap();
+        let serde_event_id = serde_json::to_string(&event_id_d1).unwrap();
+        assert_eq!(serde_event_id, "\"BTC_USD_01JUL25d\"");
+
+        let de_ser_event_id: EventId = serde_json::from_str(&serde_event_id).unwrap();
+        assert_eq!(event_id_d1, de_ser_event_id);
+    }
+
+    #[test]
+    fn test_event_id_parse_failures() {
+        assert!(matches!(
+            "BTC_USD_01JUL25f17173360005".parse::<EventId>(),
+            Err(ParsingError::InvalidLength {
+                expected: SPOT_LENGTH,
+                actual: 27,
+            })
+        ));
+
+        assert!(matches!(
+            "BTC_USD_BONJOURf1717336000".parse::<EventId>(),
+            Err(ParsingError::InvalidLength {
+                expected: SPOT_LENGTH,
+                actual: 26,
+            })
+        ));
+        assert!(matches!(
+            "BTC_USD_BONJOURdid".parse::<EventId>(),
+            Err(ParsingError::InvalidLength {
+                expected: SPOT_LENGTH,
+                actual: 18,
+            })
+        ));
     }
 }
